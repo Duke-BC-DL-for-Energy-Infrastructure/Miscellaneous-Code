081ff608cbcd892ee9ddcc737540cc7c02f5c8c5
Tyler Feldman <tylerfeldman321@gmail.com>
2021-01-25 20:30:56 -0500

    Upload code

---
+++ cross_domain_file_paths.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 17:14:56 2021
+
+@author: TylerFeldman
+"""
+import random
+import glob
+
+training_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Splitting Images into Categories\deserts\*.jpg')
+validation_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Splitting Images into Categories\farmlands\*.jpg')
+synthetic_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Synthetic Imagery\synthetic_images\farm*.png')
+
+random.shuffle(training_paths)
+random.shuffle(validation_paths)
+random.shuffle(synthetic_paths)
+
+training_imgs = open('training_img_paths.txt', 'w')
+training_lbls = open('training_lbl_paths.txt', 'w')
+for img in training_paths:
+    training_imgs.write('../data/images/' + img.split('\\')[-1] + '\n')
+    training_lbls.write('../data/labels/' + img.split('\\')[-1].replace('.jpg', '.txt') + '\n')
+training_imgs.close()
+training_lbls.close()
+
+validation_imgs = open('val_img_paths.txt', 'w')
+validation_lbls = open('val_lbl_paths.txt', 'w')
+for img in validation_paths:
+    validation_imgs.write('../data/images/' + img.split('\\')[-1] + '\n')
+    validation_lbls.write('../data/labels/' + img.split('\\')[-1].replace('.jpg', '.txt') + '\n')
+validation_imgs.close()
+validation_lbls.close()
+        
+synthetic_imgs = open('syn_img_paths.txt', 'w')
+synthetic_lbls = open('syn_lbl_paths.txt', 'w')
+for img in synthetic_paths:
+    synthetic_imgs.write('../data/synthetic_images/' + img.split('\\')[-1] + '\n')
+    synthetic_lbls.write('../data/synthetic_labels/' + img.split('\\')[-1].replace('.png', '.txt') + '\n')
+synthetic_imgs.close()
+synthetic_lbls.close()
+        
