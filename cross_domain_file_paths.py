43ed0f87fe5745315c1982e350afb70ded0c0f03
Tyler Feldman <tylerfeldman321@gmail.com>
2021-01-26 10:13:19 -0500

    Added variable for splitting paths

--- cross_domain_file_paths.py
+++ cross_domain_file_paths.py
@@ -10,6 +10,7 @@ import glob
 training_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Splitting Images into Categories\deserts\*.jpg')
 validation_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Splitting Images into Categories\farmlands\*.jpg')
 synthetic_paths = glob.glob(r'C:\Users\Tyler Feldman\Box\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Synthetic Imagery\synthetic_images\farm*.png')
+separator = '\\' # Character used to separate directories in the paths that come from calling glob
 
 random.shuffle(training_paths)
 random.shuffle(validation_paths)
@@ -18,24 +19,24 @@ random.shuffle(synthetic_paths)
 training_imgs = open('training_img_paths.txt', 'w')
 training_lbls = open('training_lbl_paths.txt', 'w')
 for img in training_paths:
-    training_imgs.write('../data/images/' + img.split('\\')[-1] + '\n')
-    training_lbls.write('../data/labels/' + img.split('\\')[-1].replace('.jpg', '.txt') + '\n')
+    training_imgs.write('../data/images/' + img.split(separator)[-1] + '\n')
+    training_lbls.write('../data/labels/' + img.split(separator)[-1].replace('.jpg', '.txt') + '\n')
 training_imgs.close()
 training_lbls.close()
 
 validation_imgs = open('val_img_paths.txt', 'w')
 validation_lbls = open('val_lbl_paths.txt', 'w')
 for img in validation_paths:
-    validation_imgs.write('../data/images/' + img.split('\\')[-1] + '\n')
-    validation_lbls.write('../data/labels/' + img.split('\\')[-1].replace('.jpg', '.txt') + '\n')
+    validation_imgs.write('../data/images/' + img.split(separator)[-1] + '\n')
+    validation_lbls.write('../data/labels/' + img.split(separator)[-1].replace('.jpg', '.txt') + '\n')
 validation_imgs.close()
 validation_lbls.close()
         
 synthetic_imgs = open('syn_img_paths.txt', 'w')
 synthetic_lbls = open('syn_lbl_paths.txt', 'w')
 for img in synthetic_paths:
-    synthetic_imgs.write('../data/synthetic_images/' + img.split('\\')[-1] + '\n')
-    synthetic_lbls.write('../data/synthetic_labels/' + img.split('\\')[-1].replace('.png', '.txt') + '\n')
+    synthetic_imgs.write('../data/synthetic_images/' + img.split(separator)[-1] + '\n')
+    synthetic_lbls.write('../data/synthetic_labels/' + img.split(separator)[-1].replace('.png', '.txt') + '\n')
 synthetic_imgs.close()
 synthetic_lbls.close()
         
