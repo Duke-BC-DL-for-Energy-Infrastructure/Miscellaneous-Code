fd44f9d11153a815c93e6d1b0aeabc55d296396e
tylerfeldman321 <tylerfeldman321@gmail.com>
2021-04-19 22:10:05 -0400

    Clean up file

--- parse_geojson.py
+++ parse_geojson.py
@@ -14,17 +14,27 @@ regions = {'SW' : ['CA', 'AZ', 'TX', 'NM', 'NV', 'UT', 'CO'],
               'NE': ['VT', 'MD', 'ME', 'NH', 'PA', 'NJ', 'NY', 'MA', 'DE'],
               'NW': ['WA', 'ID', 'OR', 'MT'],
               'EM': ['MI', 'MN', 'MO', 'WI', 'IN', 'IA', 'IL', 'OH'],
-              'WM': ['ND', 'OK', 'KS', 'SD', 'NE']}
+              'WM': ['ND', 'OK', 'KS', 'SD', 'NE'],
+              'ALL': ['CA', 'AZ', 'TX', 'NM', 'NV', 'UT', 'CO', 
+                        'VT', 'MD', 'ME', 'NH', 'PA', 'NJ', 'NY', 'MA', 'DE',
+                        'WA', 'ID', 'OR', 'MT', 'MI', 'MN', 'MO', 'WI', 'IN', 'IA', 'IL', 'OH',
+                        'ND', 'OK', 'KS', 'SD', 'NE']}
 
+# INPUT: 
+target_region = 'ALL' # Which region to get lat/lon info on
+separator = '\\' # Character used to separate directories on your operating system
+
+
+# Collect image paths. Change directory path to your own directory
 EM_image_paths = glob.glob(r'C:\Users\Student\Box Sync\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Cross Domain\EM_new\images\*.jpg')
 NE_image_paths = glob.glob(r'C:\Users\Student\Box Sync\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Cross Domain\NE_new\images\*.jpg')
 NW_image_paths = glob.glob(r'C:\Users\Student\Box Sync\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Cross Domain\NW_new\images\*.jpg')
+SW_image_paths = glob.glob(r'C:\Users\sarah\Box Sync\Bass Connections 2020-2021\Wind Turbine Object Detection Dataset\Data\images\*.jpg')
+image_paths = EM_image_paths + NE_image_paths + NW_image_paths + SW_image_paths # Concatentate all the lists of paths
 
-# Paths and names for all of the 608x608 images
-image_paths = EM_image_paths + NE_image_paths + NW_image_paths # Concatentate all the lists of paths
-image_names = [path.split('\\')[-1].split('.')[0] for path in image_paths if path.split('\\')[-1].split('.')[0].split('_')[0] == 'naip'] # Get just the naip image names
-image_names = [img for img in image_names if (get_region(img) == 'NE')]
-
+# Get just the naip image names. This is because the geojson file only has info on the original naip images (the ones that have "naip" in the file name)
+image_names = [path.split(separator)[-1].split('.')[0] for path in image_paths if path.split(separator)[-1].split('.')[0].split('_')[0] == 'naip'] 
+image_names = [img for img in image_names if (get_region(img) == target_region)] # Collect the images that are within the target region
 
 parent_image_coordinates = {}
 
